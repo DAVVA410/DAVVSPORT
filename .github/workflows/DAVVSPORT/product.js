dd528aea063cf6a223a1cdf68954240b880ad09d
DAVVA410 <mochamaddava987@gmail.com>
2025-09-17 12:59:45 +0700

    Delete .github/workflows/DAVVSPORT directory

--- .github/workflows/DAVVSPORT/product.js
+++
@@ -1,8 +0,0 @@
-let currentImage = 0;
-const images = document.querySelectorAll('.image-slider img');
-
-function showImage(index) {
-  images[currentImage].classList.remove('active');
-  currentImage = index;
-  images[currentImage].classList.add('active');
-}
